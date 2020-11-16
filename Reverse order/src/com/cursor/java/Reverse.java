5fcd6257a1b3fcd50b479cd89c5017c1c10c4a3f
Dmytro <65898747+fill19@users.noreply.github.com>
2020-11-16 20:07:30 +0200

    Update Reverse.java

--- Reverse order/src/com/cursor/java/Reverse.java
+++ Reverse order/src/com/cursor/java/Reverse.java
@@ -2,28 +2,27 @@ package com.cursor.java;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 
 
-public class Reverse {
+public class Test {
     public static void main(String[] args) {
 
         //First Task
-        Comparator<Integer> reverseOrderComparator = Collections.reverseOrder();
+
         Integer[] num = {2, 3, 1, 7, 11};
-        Arrays.sort(num, (Comparator<Integer>) (Object) reverseOrderComparator);
+        Arrays.sort(num);
         System.out.println("Reverse order of massive: ");
         printingOfArray(num);
 
         //Second Task
-         Integer[] daim = {12, 53, -17, 5, -53};
+        Integer[] diam = {12, 53, -17, 5, -53};
         System.out.println("The sum of positive values:");
-        System.out.println(theSum(daim));
+        System.out.println(theSum(diam));
 
         //Third Task
         Integer[] chag = {2, 4, 7, 16};
         System.out.println("The avarage of values: ");
-        theAvarage(chag);
+        theAverage(chag);
 
 
         //Forth Task
@@ -35,31 +34,32 @@ public class Reverse {
 
     private static void printingOfArray(Integer[] num) {
         for (int i = 0; i < num.length; i++) {
+            Arrays.sort(num, Collections.reverseOrder());
             System.out.println(num[i]);
         }
     }
 
 
-    private static void theAvarage(int[] chag) {
-        int totalChag = 0;
-        for (int i = 0; i < chag.length; i++) {
-            totalChag = totalChag + chag[i];
+    private static void theAverage(Integer[] chug) {
+        int totalChug = 0;
+        for (int i = 0; i < chug.length; i++) {
+            totalChug = totalChug + chug[i];
         }
-        int avarage = totalChag / chag.length;
+        int avarage = totalChug / chug.length;
         System.out.println(avarage);
     }
 
-    private static int theSum(int[] daim) {
-        int  sum = 0;
-        for (int i = 0; i < daim.length; i++) {
-            if (daim[i] > 0)
-                sum = sum + daim[i];
+    private static int theSum(Integer[] diam) {
+        int sum = 0;
+        for (int i = 0; i < diam.length; i++) {
+            if (diam[i] > 0)
+                sum = sum + diam[i];
         }
 
         return sum;
     }
 
-    private static void duplicatedValues(int[] dex) {
+    private static void duplicatedValues(Integer[] dex) {
         for (int i = 0; i < dex.length; i++) {
             for (int b = 0; b < dex.length; b++) {
                 if (b == i)
@@ -67,7 +67,7 @@ public class Reverse {
 
                 if (dex[b] == dex[i]) {
                     dex[b] = 0;
-                    
+
                 }
 
             }
@@ -77,6 +77,7 @@ public class Reverse {
     }
 
 }
+
 
 
 
