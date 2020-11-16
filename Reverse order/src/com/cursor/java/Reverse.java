2fb92e22540ee491283c8fbbea090afd64236e7b
Dmytro <65898747+fill19@users.noreply.github.com>
2020-11-16 14:46:31 +0200

    Update Reverse.java

--- Reverse order/src/com/cursor/java/Reverse.java
+++ Reverse order/src/com/cursor/java/Reverse.java
@@ -3,7 +3,7 @@ package com.cursor.java;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.*;
+
 
 public class Reverse {
     public static void main(String[] args) {
